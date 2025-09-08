fe38f64238c27d7e93ab4962a6b2bb6dff636fa7
abhi-thecoder <abhishekkumarmahto2005@gmail.com>
2025-09-08 18:01:32 +0530

    Updated api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL: "http://localhost:5001/",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: "http://localhost:5001/",
   headers: {
     'Content-Type': 'application/json' 
   }
